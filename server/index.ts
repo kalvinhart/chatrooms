edff66f2e1dfe85aa7f0d14997ba9a3c30e46855
Kalvin <kalvinhart@hotmail.co.uk>
2022-09-08 12:24:48 +0100

    added socket.io

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
+const { createServer } = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 const app = express();
-
 app.use(cors());
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:3000"],
+  },
+});
+
+io.on("connection", (socket: any) => {
+  console.log("Connected: ", socket.id);
+});
 
 const PORT = process.env.PORT ?? 5050;
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
