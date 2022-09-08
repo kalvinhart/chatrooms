a3d8aa967fcb2ca75f0305594ecbcd82ae3e0c75
Kalvin <kalvinhart@hotmail.co.uk>
2022-09-08 12:40:35 +0100

    added socket server skeleton

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,14 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
-const { createServer } = require("http");
-const cors = require("cors");
-const { Server } = require("socket.io");
+import cors from "cors";
+
+import { makeServer } from "./utils/makeServer";
 
 const app = express();
 app.use(cors());
 
-const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: {
-    origin: ["http://localhost:3000"],
-  },
-});
+const { httpServer, io } = makeServer(app);
 
 io.on("connection", (socket: any) => {
   console.log("Connected: ", socket.id);
