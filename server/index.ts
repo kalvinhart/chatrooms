2c696e6efb6fba69618cbdeeea4099f9970add47
Kalvin <kalvinhart@hotmail.co.uk>
2022-09-13 12:14:32 +0100

    added middleware to handle json data

--- server/index.ts
+++ server/index.ts
@@ -10,6 +10,8 @@ import roomRoutes from "./routes/roomRoutes";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const { httpServer, io } = makeServer(app);
 
